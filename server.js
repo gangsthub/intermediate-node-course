62c4dbedd5142dcaae8a9bac1675faf6f42680ef
gangsthub <paulmelero@gmail.com>
2021-03-05 08:41:14 +0100

    added Create and Read methods

--- server.js
+++ server.js
@@ -1,30 +1,67 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const port = 8000
+const app = express()
 
-app.use(bodyParser.json());
+const User = require('./models/User')
+mongoose.connect('mongodb://localhost/userData')
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
+app.post('/users', (req, res) => {
+  const { newData } = req.body
+  User.create(
+    {
+      name: newData && newData.name,
+      email: newData && newData.email,
+      password: newData && newData.password,
+    },
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err })
+      } else if (!data) {
+        res.json({ success: false, message: 'Not Found' })
+      } else {
+        res.json({ success: true, data: data })
+      }
+    }
+  )
 })
 
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+app
+  .route('/users/:id')
+  // READ
+  .get((req, res) => {
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err,
+        })
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: 'Not Found',
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data,
+        })
+      }
+    })
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  })
